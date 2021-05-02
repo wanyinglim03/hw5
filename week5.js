21b318eceb7d52455841636e27d9c1b05481a2da
Wan Ying Lim <wan.lim@kellogg.northwestern.edu>
2021-05-02 17:01:48 -0500

    v1

--- week5.js
+++ week5.js
@@ -20,17 +20,19 @@ window.addEventListener('DOMContentLoaded', async function() {
     // When the "get weather" button is clicked:
     getWeatherButton.addEventListener(`click`, async function(event) {
   
-    
       // - Ignore the default behavior of the button
       event.preventDefault()
-      // - Get a reference to the element containing the user-entered location
+      // - Get a reference to the element containing the user-entered location and days
       let locationInput = document.querySelector(`#location`)
+      let daysInput = document.querySelector(`#days`)
   
-      // - Get the user-entered location from the element's value
+      // - Get the user-entered location and days from the element's value
       let location = locationInput.value
+      let days = daysInput.value
+
   
       // - Check to see if the user entered anything; if so:
-      if (location.length > 0) {
+      if (location.length > 0 & days > 0) {
       
         // - Construct a URL to call the WeatherAPI.com API
         let url = `https://api.weatherapi.com/v1/forecast.json?key=78407a7389a545399d2153920212704&q=${location}&days=3`
@@ -55,14 +57,47 @@ window.addEventListener('DOMContentLoaded', async function() {
           <div class="text-center space-y-2">
             <div class="font-bold text-3xl">Current Weather for ${interpretedLocation.name}, ${interpretedLocation.region}</div>
             <div class="font-bold">
-              <img src="https://cdn.weatherapi.com/weather/64x64/day/116.png" class="inline-block">
-              <span class="temperature">60</span>° 
+              <img src="https:${currentWeather.condition.icon}" class="inline-block">
+              <span class="temperature">${currentWeather.temp_f}</span>° 
               and
-              <span class="conditions">Partly Cloudy</span>
+              <span class="conditions">${currentWeather.condition.text}</span>
             </div>
           </div>
         `
+
+      // - Store a reference to the "forecast" element
+      let forecastElement = document.querySelector(`.forecast`)
+      
+      // - Fill the forecast element with the number of forecast days
+      forecastElement.innerHTML = `
+        <div class="text-center space-y-8">
+          <div class="font-bold text-3xl">${days} Day Forecast</div>
+        </div>
+      `
+
+        // - Loop through the forecast data 
+        for (i=0; i< dailyForecast.forecastday.length; i++){
+
+          // - Store icon, date, high and low temperature, and a summary of the conditions
+          let icon = dailyForecast.forecastday[i].day.condition.icon
+          let date = dailyForecast.forecastday[i].date
+          let highLow = `High ${dailyForecast.forecastday[i].day.maxtemp_f}° – Low ${dailyForecast.forecastday[i].day.mintemp_f}°`
+          let condition = dailyForecast.forecastday[i].day.condition.text
+
+          // Insert HTML into the "forecast" element using the data stored above
+          forecastElement.insertAdjacentHTML(`beforeend`, `
+          <div class="text-center space-y-8">
+              <div>
+                <img src="https:${icon}" class="mx-auto">
+                <h1 class="text-2xl text-bold text-gray-500">${date}</h1>
+                <h2 class="text-xl">${highLow}</h2>
+                <p class="text-gray-500">${condition}</h1>
+              </div>
+            </div>
+          `)
+        }
       }
     })
   })
-      
+
+
